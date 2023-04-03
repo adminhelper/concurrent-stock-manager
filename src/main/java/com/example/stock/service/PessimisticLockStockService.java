b6e3a36b1b47a91447dfd6c3360a6666d8a0446f
adminhelper <201401339hs@gmail.com>
2023-04-03 23:05:08 +0900

    feat : Pessimistic Lock

--- src/main/java/com/example/stock/service/PessimisticLockStockService.java
+++ src/main/java/com/example/stock/service/PessimisticLockStockService.java
@@ -1,28 +1,25 @@
 package com.example.stock.service;
 
- import com.example.stock.domain.Stock;
+import com.example.stock.domain.Stock;
 import com.example.stock.repository.StockRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
-public class StockService {
+public class PessimisticLockStockService {
 
     private final StockRepository stockRepository;
 
-    public StockService(StockRepository stockRepository) {
+    public PessimisticLockStockService(StockRepository stockRepository) {
         this.stockRepository = stockRepository;
     }
 
-    //@Transactional
-    public synchronized void decrease(Long id, Long quantity) {
-        // get stock
-        // 재고감소
-        // 저장
-        Stock stock = stockRepository.findById(id).orElseThrow();
+    @Transactional
+    public void decrease(Long id, Long quantity) {
+        Stock stock = stockRepository.findByIdWithPessimisticLock(id);
+
         stock.decrease(quantity);
 
         stockRepository.saveAndFlush(stock);
     }
-
 }

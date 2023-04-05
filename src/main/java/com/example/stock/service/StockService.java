038f60b395795cd930e83bba1ae1564ffcb83d1f
adminhelper <201401339hs@gmail.com>
2023-04-05 21:27:06 +0900

    feat : Named Lock

--- src/main/java/com/example/stock/service/StockService.java
+++ src/main/java/com/example/stock/service/StockService.java
@@ -3,7 +3,8 @@ package com.example.stock.service;
  import com.example.stock.domain.Stock;
 import com.example.stock.repository.StockRepository;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
+ import org.springframework.transaction.annotation.Propagation;
+ import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class StockService {
@@ -14,12 +15,13 @@ public class StockService {
         this.stockRepository = stockRepository;
     }
 
-    //@Transactional
+    @Transactional(propagation =  Propagation.REQUIRES_NEW)
     public synchronized void decrease(Long id, Long quantity) {
         // get stock
         // 재고감소
         // 저장
         Stock stock = stockRepository.findById(id).orElseThrow();
+
         stock.decrease(quantity);
 
         stockRepository.saveAndFlush(stock);
